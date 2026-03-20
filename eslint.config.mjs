a394c6144dc63612c2bb5e510f5268aed1f30894
tribecon1 <bentl24@byu.edu>
2026-03-19 20:25:42 -0600

    ignore K6 test in lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
+  { ignores: ['loadTests/**'] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.node } },
   { languageOptions: { globals: globals.jest } },
