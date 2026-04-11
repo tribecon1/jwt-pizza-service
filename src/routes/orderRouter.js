f27f50380ee25850fd0d9c16190051d6f1cd25bd
tribecon1 <bentl24@byu.edu>
2026-04-11 12:30:00 -0600

    Removed Chaos Monkey test endpoint

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -74,25 +74,25 @@ orderRouter.get(
   })
 );
 
-let enableChaos = false;
-orderRouter.put(
-  '/chaos/:state',
-  authRouter.authenticateToken,
-  asyncHandler(async (req, res) => {
-    if (req.user.isRole(Role.Admin)) {
-      enableChaos = req.params.state === 'true';
-    }
+// let enableChaos = false;
+// orderRouter.put(
+//   '/chaos/:state',
+//   authRouter.authenticateToken,
+//   asyncHandler(async (req, res) => {
+//     if (req.user.isRole(Role.Admin)) {
+//       enableChaos = req.params.state === 'true';
+//     }
 
-    res.json({ chaos: enableChaos });
-  })
-);
+//     res.json({ chaos: enableChaos });
+//   })
+// );
 
-orderRouter.post('/', (req, res, next) => {
-  if (enableChaos && Math.random() < 0.5) {
-    throw new StatusCodeError('Chaos monkey', 500);
-  }
-  next();
-});
+// orderRouter.post('/', (req, res, next) => {
+//   if (enableChaos && Math.random() < 0.5) {
+//     throw new StatusCodeError('Chaos monkey', 500);
+//   }
+//   next();
+// });
 
 
 // createOrder
